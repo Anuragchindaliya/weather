2a77a743da4dfa246d636899f9fb01a3d9cd8577
AnuragChindaliya <ak1148383@gmail.com>
2021-05-11 01:37:59 +0530

    added suggestion while searching

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,23 +92,52 @@ const getDateTime = (unixTimeStamp) => {
 const getTemp = (val, unit) => {
 
         const flag = unit == "imperial" ? "°F" : "°C";
-        return `Temprature : ${val.temp}${flag}
-    <span>Max : ${val.temp_max}${flag}</span>
-    <span>Min : ${val.temp_min}${flag}</span>`;
+        return `
+        <div class="tempcard">
+        
+    <div>Temprature : ${val.temp}${flag}</div>
+    <div>Max : ${val.temp_max}${flag}</div>
+    <div>Min : ${val.temp_min}${flag}</div>
+    </div>
+    <div class="button button-dial mt-4">
+
+            <span class="button-dial-spoke"></span>
+            <span class="button-dial-spoke"></span>
+            <span class="button-dial-spoke"></span>
+            <span class="button-dial-spoke"></span>
+            <span class="button-dial-spoke"></span>
+            <span class="button-dial-spoke"></span>
+
+            <div class="button-dial-top"></div>
+            <div class="button-dial-label">
+              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px"
+                y="0px" viewBox="0 0 100 125" enable-background="new 0 0 100 100" xml:space="preserve" class="meter">
+                <g>
+                  <path
+                    d="M60.333,68.349V11.111C60.333,4.984,55.349,0,49.222,0c-6.126,0-11.111,4.984-11.111,11.111v57.237   c-4.15,3.323-6.667,8.397-6.667,13.874c0,9.802,7.975,17.777,17.777,17.777C59.024,100,67,92.024,67,82.223   C67,76.746,64.482,71.672,60.333,68.349z M44.778,48.363h4.444v-4.444h-4.444V35.03h4.444v-4.444h-4.444v-8.889h4.444v-4.444   h-4.444v-6.142c0-2.455,1.99-4.444,4.444-4.444c2.455,0,4.445,1.989,4.445,4.444v41.111h-8.889V48.363z">
+                  </path>
+                </g>
+              </svg> ${val.temp}${flag}
+            </div>
+          </div>
+    `;
     }
     // @param data = weatherInfo.{} = getWeather(selectedCity, countryDropDown.value).{};
     // and display the html
 const displayWeather = (data) => {
+        const temp = getTemp(data.main, "cell");
         const weatherWidget = `
+        
+        
+
+
         <div class="flex-button-container">
         <button class="button button-large units">
         ${getDateTime(data.dt)}<br/>
         City : ${data.name} ${data.sys.country}
     </button>
 
-        <button class="button button-large" id="tempcard">
-        ${getTemp(data.main,"cell")}
-        </button>
+        
     
     ${data.weather.map(
             (w) => `
@@ -119,15 +148,21 @@ const displayWeather = (data) => {
             </button>`
         ).join(" ")} 
         </div>
-        <a class="btn btn-primary button unitLink mt-2 active" data-list="metric">C </a>
-                    <a class="btn btn-primary button unitLink mt-2 mx-1" data-list="imperial">F </a>
-                    `;
+        <button class="button button-large w-100 mt-4" id="tempcard">
+        ${getTemp(data.main,"cell")}
+        </button>
+                    
+                    
+          
+          <a class="btn btn-primary button unitLink mt-2 active" data-list="metric">C </a>
+          <a class="btn btn-primary button unitLink mt-2 mx-1" data-list="imperial">F </a>
+          `;
     tempCont.innerHTML = weatherWidget;
 };
 
 const dispLoader = () => {
     return `
-    <div class="spinner-grow text-primary m-auto" role="status">
+    <div class="spinner-grow text-primary m-auto d-block" role="status">
       <span class="sr-only">Loading...</span>
   </div>`;
 };
@@ -136,6 +171,13 @@ const countryDropDown = document.querySelector("#countrylist");
 const stateDropDown = document.querySelector("#statelist");
 const cityDropDown = document.querySelector("#citylist");
 const tempCont = document.querySelector("#tempContainer");
+
+
+const countryValue = document.querySelector("#countryValue");
+const stateValue = document.querySelector("#stateValue");
+const cityValue = document.querySelector("#cityValue");
+
+
 
 
 // on Document  load
@@ -147,8 +189,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     // to display option to select country
     let countriesOptions = "";
     if (countries) {
-        countriesOptions += `<option value="
-            ">Country</option>`;
         countries.forEach(country => {
             countriesOptions += `<option value="${country.iso2}">${country.name}</option>`;
         });
@@ -156,54 +196,53 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     // List state when client select country and here we using this keyword so we are not using arrow function here
-    countryDropDown.addEventListener('change', async function () {
+    countryValue.addEventListener('change', async function () {
         const selectedCountryCode = this.value;
         const states = await getCountries("states", selectedCountryCode);
 
 
         let stateOptions = '';
         if (states) {
-            stateOptions += `<option value="
-                ">State</option>`;
+
             states.forEach(state => {
                 stateOptions += `<option value="${state.iso2}">${state.name}</option>`;
             });
             stateDropDown.innerHTML = stateOptions;
-            stateDropDown.disabled = false;
-            stateDropDown.focus();
+            stateValue.disabled = false;
+            stateValue.focus();
         }
         // console.log(states);
 
     });
 
     // list cities
-    stateDropDown.addEventListener("change", async function () {
+    stateValue.addEventListener("change", async function () {
         // for getting the value of country we can't use this keyword so we'll use option value explicity by  id
-        const selectedCountryCode = countryDropDown.value;
+        const selectedCountryCode = countryValue.value;
         const selectedStateCode = this.value;
         const cities = await getCountries("cities", selectedCountryCode, selectedStateCode);
         // console.log(cities);
 
         let citiesOptions = '';
         if (cities) {
-            citiesOptions += `<option value="">City</option>`;
+
             cities.forEach(city => {
                 citiesOptions += `<option value="${city.name}">${city.name}</option>`;
             })
         }
         cityDropDown.innerHTML = citiesOptions;
-        cityDropDown.disabled = false;
-        cityDropDown.focus();
+        cityValue.disabled = false;
+        cityValue.focus();
 
     });
 
     // select city value
-    cityDropDown.addEventListener("change", async function () {
+    cityValue.addEventListener("change", async function () {
         const selectedCity = this.value;
         // console.log(city);
         tempCont.innerHTML = dispLoader();
         // getWeather is also return Promise so we will use async await
-        const weatherInfo = await getWeather(selectedCity, countryDropDown.value);
+        const weatherInfo = await getWeather(selectedCity, countryValue.value);
 
         // console.log(weatherInfo);
 
@@ -219,7 +258,7 @@ document.addEventListener("click", async (e) => {
 
         const unitValue = e.target.getAttribute("data-list");
 
-        const selectedCountryCode = countryDropDown.value;
+        const selectedCountryCode = countryValue.value;
         const selectedCity = cityDropDown.value;
 
 
